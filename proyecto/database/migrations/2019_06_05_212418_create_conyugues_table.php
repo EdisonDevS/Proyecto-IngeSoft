42f73da7d93ae2998d1d33a16a23c9aaaabb4f0f
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-06-06 17:32:34 -0500

    Organizadas varias de las migraciones, queda pendiente organizar los formularios para recibir los datos flatantes, en especial el de registro de usuarios que debe recibir los datos de los conyugues

---
+++ proyecto/database/migrations/2019_06_05_212418_create_conyugues_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConyuguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conyugues', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('gender');
+            $table->unsignedBigInteger('person_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conyugues');
+    }
+}
--- proyecto/database/migrations/2019_06_05_212436_create_guests_table.php
+++ proyecto/database/migrations/2019_06_05_212436_create_guests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGuestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('guests', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email')->unique();
-            $table->double('budget')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('person_id')->nullable();
+            $table->unsignedBigInteger('conyugue_id')->nullable();
             $table->timestamps();
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('conyugue_id')->references('id')->on('conyugues');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('guests');
     }
 }
