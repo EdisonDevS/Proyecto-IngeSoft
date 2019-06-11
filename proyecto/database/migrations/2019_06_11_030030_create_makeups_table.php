846df226071701f0e775b482030593cee6059b3a
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-06-10 22:22:22 -0500

    Creadas las tablas hijas de PreviousOrganization con sus respectivas llaves foraneas

---
+++ proyecto/database/migrations/2019_06_11_030030_create_makeups_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMakeupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('makeups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('previous_organization_id')->references('id')->on('previous_organizations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('makeups');
+    }
+}
