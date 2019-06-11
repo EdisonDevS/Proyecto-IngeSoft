4a41f47685cd2df6deea4513f0c7eb1b6ceaa9cd
Jhon Edison Giraldo <jhonedisongiraldo99@gmail.com>
2019-06-10 19:41:49 -0500

    Creada relacion de la clase padre package a sus clases hijas

--- proyecto/app/Package.php
+++ proyecto/app/Package.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model
 {
-    
+    public function ceremony()
+    {
+    	return $this->hasOne('App\ceremony');
+    }
+
+    public function 	reception()
+    {
+    	return $this->hasOne('App\Reception');
+    }
+
+    public function honeymoon()
+    {
+    	return $this->hasOne('App\Honeymoon');
+    }
+
+    public function previousOrganization()
+    {
+    	return $this->hasOne('PreviousOrganization')
+    }
 }
