3ea8dad50e2e0c32500ca42a9f3bb9023680d9eb
root <niko.orozc@gmail.com>
2019-07-02 16:31:28 -0500

    hi

--- proyecto/resources/views/users/profile.blade.php
+++ proyecto/resources/views/users/profile.blade.php
@@ -22,7 +22,7 @@
                                         <label for="name1" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="name1" type="text" class="form-control @error('name1') is-invalid @enderror" name="name1" value="{{ old('name1') }}" required autocomplete="name" autofocus>
+                                            <input id="name1" type="text" class="form-control @error('name1') is-invalid @enderror" name="name1" value="{{ auth()->user()->conyugues[0]->name }}" required autocomplete="name" autofocus>
 
                                             @error('name1')
                                                 <span class="invalid-feedback" role="alert">
@@ -36,7 +36,7 @@
                                         <label for="last_name1" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="last_name1" type="text" class="form-control @error('last_name1') is-invalid @enderror" name="last_name1" value="{{ old('last_name1') }}" required autocomplete="last_name1" autofocus>
+                                            <input id="last_name1" type="text" class="form-control @error('last_name1') is-invalid @enderror" name="last_name1" value="{{ auth()->user()->conyugues[0]->last_name }}" required autocomplete="last_name1" autofocus>
 
                                             @error('last_name1')
                                                 <span class="invalid-feedback" role="alert">
@@ -50,7 +50,7 @@
                                         <label for="document1" class="col-md-4 col-form-label text-md-right">{{ __('Documento') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="document1" type="text" class="form-control @error('document1') is-invalid @enderror" name="document1" value="{{ old('document1') }}" required autocomplete="document1" autofocus>
+                                            <input id="document1" type="text" class="form-control @error('document1') is-invalid @enderror" name="document1" value="{{ auth()->user()->conyugues[0]->document }}" required autocomplete="document1" autofocus>
 
                                             @error('document1')
                                                 <span class="invalid-feedback" role="alert">
@@ -64,7 +64,7 @@
                                         <label for="gender1" class="col-md-4 col-form-label text-md-right">{{ __('Genero') }}</label>
 
                                         <div class="col-md-6">
-                                            <select class="form-control" id="gender1" name="gender1" value="{{ old('gender1') }}">
+                                            <select class="form-control" id="gender1" name="gender1" value="{{ auth()->user()->conyugues[0]->gender }}">
                                                 <option value="Hombre">Hombre</option>
                                                 <option value="Mujer">Mujer</option>
                                                 <option value="Otro">Otro</option>
@@ -94,7 +94,7 @@
                                         <label for="name2" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="name2" type="text" class="form-control @error('name2') is-invalid @enderror" name="name2" value="{{ old('name2') }}" required autocomplete="name" autofocus>
+                                            <input id="name2" type="text" class="form-control @error('name2') is-invalid @enderror" name="name2" value="{{auth()->user()->conyugues[1]->name}}" required autocomplete="name" autofocus>
 
                                             @error('name2')
                                                 <span class="invalid-feedback" role="alert">
@@ -108,7 +108,7 @@
                                         <label for="last_name2" class="col-md-4 col-form-label text-md-right">{{ __('Apellido') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="last_name2" type="text" class="form-control @error('last_name2') is-invalid @enderror" name="last_name2" value="{{ old('last_name2') }}" required autocomplete="last_name2" autofocus>
+                                            <input id="last_name2" type="text" class="form-control @error('last_name2') is-invalid @enderror" name="last_name2" value="{{ auth()->user()->conyugues[1]->last_name }}" required autocomplete="last_name2" autofocus>
 
                                             @error('last_name2')
                                                 <span class="invalid-feedback" role="alert">
@@ -122,7 +122,7 @@
                                         <label for="document2" class="col-md-4 col-form-label text-md-right">{{ __('Documento') }}</label>
 
                                         <div class="col-md-6">
-                                            <input id="document1" type="text" class="form-control @error('document2') is-invalid @enderror" name="document2" value="{{ old('document2') }}" required autocomplete="document2" autofocus>
+                                            <input id="document1" type="text" class="form-control @error('document2') is-invalid @enderror" name="document2" value="{{ auth()->user()->conyugues[1]->document }}" required autocomplete="document2" autofocus>
 
                                             @error('document2')
                                                 <span class="invalid-feedback" role="alert">
@@ -136,7 +136,7 @@
                                         <label for="gender2" class="col-md-4 col-form-label text-md-right">{{ __('Genero') }}</label>
 
                                         <div class="col-md-6">
-                                            <select class="form-control" id="gender2" name="gender2" value="{{ old('gender2') }}">
+                                            <select class="form-control" id="gender2" name="gender2" value="{{ auth()->user()->conyugues[0]->gender }}">
                                                 <option value="Hombre">Hombre</option>
                                                 <option value="Mujer">Mujer</option>
                                                 <option value="Otro">Otro</option>
@@ -160,7 +160,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Telefono') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ auth()->user()->phone }}" required autocomplete="phone" autofocus>
 
                                 @error('phone')
                                     <span class="invalid-feedback" role="alert">
@@ -174,7 +174,7 @@
                             <label for="budget" class="col-md-4 col-form-label text-md-right">{{ __('Presupuesto') }}</label>
 
                             <div class="col-md-6">
-                                <input id="budget" type="text" class="form-control @error('budget') is-invalid @enderror" name="budget" value="{{ old('budget') }}" required autocomplete="budget" autofocus>
+                                <input id="budget" type="text" class="form-control @error('budget') is-invalid @enderror" name="budget" value="{{ auth()->user()->budget }}" required autocomplete="budget" autofocus>
 
                                 @error('budget')
                                     <span class="invalid-feedback" role="alert">
@@ -188,7 +188,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ auth()->user()->email }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
